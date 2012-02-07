3c80500da311d853c1110fd222e4e1e3008c0df2
Thiago Colucci <ticolucci@gmail.com>
2012-02-07 16:58:03 -0200

    fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Spree::Core::Engine.routes.prepend do
-  post "pag_seguro/confirm", :to => "pag_seguro#confirm"
-  get "pag_seguro/callback", :to => "pag_seguro#callback"
+  match "pag_seguro/notify", :to => "pag_seguro#notify", method: :post
+  match "pag_seguro/callback", :to => "pag_seguro#callback", method: :get
   resources :payment_notifications, :only => [:create]
 end
