795159e5be7b49099af3f7f7c0b9bb033f2a333e
Stefano Diem Benatti <stefano.diem@gmail.com>
2012-02-23 16:16:10 -0200

    Making actions appear on payments index, so that admind can set a payment as complete

--- app/models/spree/pag_seguro_payment.rb
+++ app/models/spree/pag_seguro_payment.rb
@@ -30,5 +30,31 @@ module Spree
       self.date = pag_seguro_payment.date
       self.save
     end
+    
+    def actions
+      %w{capture void}
+    end
+
+    # Indicates whether its possible to capture the payment
+    def can_capture?(payment)
+      ['processing', 'checkout', 'pending'].include?(payment.state)
+    end
+
+    # Indicates whether its possible to void the payment.
+    def can_void?(payment)
+      payment.state != 'void'
+    end
+
+    def capture(payment)
+      payment.update_attribute(:state, 'pending') if payment.state == 'checkout'
+      payment.complete
+      true
+    end
+
+    def void(payment)
+      payment.update_attribute(:state, 'pending') if payment.state == 'checkout'
+      payment.void
+      true
+    end
   end
 end
--- app/models/spree/payment_method/pag_seguro_method.rb
+++ app/models/spree/payment_method/pag_seguro_method.rb
@@ -6,31 +6,5 @@ module Spree
     def payment_source_class
       PagSeguroPayment
     end
-    
-    def actions
-      %w{capture void}
-    end
-
-    # Indicates whether its possible to capture the payment
-    def can_capture?(payment)
-      ['checkout', 'pending'].include?(payment.state)
-    end
-
-    # Indicates whether its possible to void the payment.
-    def can_void?(payment)
-      payment.state != 'void'
-    end
-
-    def capture(payment)
-      payment.update_attribute(:state, 'pending') if payment.state == 'checkout'
-      payment.complete
-      true
-    end
-
-    def void(payment)
-      payment.update_attribute(:state, 'pending') if payment.state == 'checkout'
-      payment.void
-      true
-    end
   end
 end
